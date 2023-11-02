b02e89cdb1f4005b29e4af05dcc8153f10986628
nika236 <nika.kapanadze.1@btu.edu.ge>
2023-11-02 10:50:07 +0400

    changed create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,17 +22,8 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    render plain:@article.inspect
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show]
-  root 'homes#home'
+  resources :articles, only: [:show, :index, :new, :create]
+  root 'homes#index'
   get 'about', to: 'homes#about'
 end
