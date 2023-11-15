ad32be255723b6ba51652fc92818327c663f55c4
nika236 <nika.kapanadze.1@btu.edu.ge>
2023-11-15 10:58:12 +0400

    BCrypt secured password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_15_060341) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_15_064703) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_060341) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
