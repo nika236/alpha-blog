aa63594bbcd026dd3b062a359d970a2db45add7d
nika236 <nika.kapanadze.1@btu.edu.ge>
2023-11-15 13:27:45 +0400

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
-  root 'articles#index'
+  resources :users, except: [:new]
+  root 'homes#home'
   get 'about', to: 'homes#about'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
